e96bdacd6a8dd611ca210435807b813fb08fe52b
Marc Hauschildt <marc.hauschildt@gmail.com>
2022-08-24 13:54:58 -0500

    Setup the intro assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java1refresher.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi I'm Marc!");
+        Book book1 = new Book();
+        Book book2 = new Book("Java A Beginner's Guide","Herbert Schildt",true,688);
+//        System.out.println(book1);
+//        System.out.println(book2);
+//        System.out.println(book2.getAuthor());
+        Book[] bookshelf = new Book[4];
+        bookshelf[0] = book1;
+        bookshelf[1] = book2;
+        bookshelf[2] = new Book();
+        bookshelf[3] = new Book();
+        for(Book book: bookshelf) {
+            System.out.println(book);
+        }
+        for(int i = 0; i <= Book.getBookCount(); i++) {
+            System.out.println(bookshelf[i]);
+        }
+
     }
 }
---
+++ src/assignments/intro/AssignmentIntroMain.java
@@ -0,0 +1,7 @@
+package assignments.intro;
+
+public class AssignmentIntroMain {
+    public static void main(String[] args) {
+
+    }
+}
