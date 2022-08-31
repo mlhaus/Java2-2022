ddf971fd405858d760fad13f3471356d9ef72450
Marc Hauschildt <marc.hauschildt@gmail.com>
2022-08-31 12:26:02 -0500

    Finished intro assignment

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,29 @@ import java1refresher.Book;
 import java.util.Arrays;
 import java.util.*;
 
+import assignments.intro.Version;
+
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("PHP and MySQL", "Welling", true, 1000);
-        Book book2 = new Book("Java A Beginner's Guide","Herbert Schildt",true,688);
-        System.out.println(book1);
-        System.out.println(book2);
-        System.out.println(book2.getAuthor());
-//        Book[] bookshelf = new Book[4];
-        List<Book> bookshelf = new ArrayList<>();
-        bookshelf.add(book1);
-        bookshelf.add(book2);
-        bookshelf.add(new Book("Java A Beginner's Guide","Herbert Schildt",true,720));
-        bookshelf.add(new Book());
-        bookshelf.remove(bookshelf.size() - 1);
-        System.out.println();
-        Collections.sort(bookshelf);
-        for(Book book: bookshelf) {
-            System.out.println(book);
+        try {
+            Version version;
+            version = new Version();
+            // version = new Version("1.2.3");
+            // version = new Version("1.2.3.4");
+            // version = new Version("1.2.3.d");
+            // version = new Version("1");
+            // version = new Version("1.1");
+            // version = new Version(1, 2, 3);
+            // version = new Version("a.b.c");
+            // version = new Version("");
+
+            System.out.println(version.getMajor());
+            System.out.println(version.getMinor());
+            System.out.println(version.getPatch());
+            System.out.println(version);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println();
-        for(int i = 0; i < bookshelf.size(); i++) {
-            System.out.println(bookshelf.get(i));
-        }
-        System.out.println();
-        bookshelf.forEach(book -> System.out.println(book));
-        bookshelf.forEach(System.out::println);
 
     }
 }
--- src/assignments/intro/Version.java
+++ src/assignments/intro/Version.java
@@ -1,4 +1,66 @@
 package assignments.intro;
 
 public class Version {
+    private int major;
+    private int minor;
+    private int patch;
+
+    public Version() {
+        patch = 1;
+    }
+
+    public Version(String version) {
+        String[] partedVersion = version.split("\\.");
+        if(partedVersion.length == 1 && partedVersion[0] == "") {
+            patch = 1;
+            return;
+        }
+        try {
+            switch(partedVersion.length) {
+                case 0:
+                    patch = 1;
+                    break;
+                case 1:
+                    major = Integer.parseInt(partedVersion[0]);
+                    break;
+                case 2:
+                    major = Integer.parseInt(partedVersion[0]);
+                    minor = Integer.parseInt(partedVersion[1]);
+                    break;
+                default:
+                    major = Integer.parseInt(partedVersion[0]);
+                    minor = Integer.parseInt(partedVersion[1]);
+                    patch = Integer.parseInt(partedVersion[2]);
+            }
+        } catch (NumberFormatException e) {
+            // throw new Exception("Error occured while parsing version!");
+            throw new IllegalArgumentException("Error occured while parsing version!", e);
+        }
+    }
+
+    public Version(int major, int minor, int patch) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+
+    public int getMajor() {
+        return major;
+    }
+
+
+    public int getMinor() {
+        return minor;
+    }
+
+
+    public int getPatch() {
+        return patch;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s.%s.%s", getMajor(), getMinor(), getPatch());
+    }
+
 }
